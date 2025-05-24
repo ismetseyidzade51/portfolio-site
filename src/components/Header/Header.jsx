832876d8cd3e2294ea49f33c91f92200f2cdfcba
seyidovismet51 <seyidovismet51@gmail.com>
2025-05-24 20:44:59 +0400

    First version

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="custom-header py-3 bg-light">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg">
+                    {/* Logo */}
+                    <a className="navbar-brand d-flex align-items-center" href="#">
+                        <span className="text-white rounded-circle d-flex justify-content-center align-items-center" style={{ width: '40px', height: '40px', fontWeight: '500', margin: '10px', backgroundColor: '#9D00FF', fontSize: '20px'}}>S</span>
+                        <span className="fw-bold" style={{ fontSize: "20px" }}>Seyidzade</span>
+                    </a>
+
+                    {/* Burger menu */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Navbar links */}
+                    <div className="collapse navbar-collapse" id="mainNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#home">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#process">Process</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#portfolio">Portfolio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#blog">Blog</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#services">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="btn custom-outline-btn ms-lg-2 mt-2 mt-lg-0" href="#contact" style={{ borderRadius: '5px'}}>
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
